42df8e5eb782cdf98612d9fddbba7d35bb2707fc
sherhn <vladimirbredihin44@gmail.com>
2025-11-13 22:23:28 +0300

    Images removed.

--- YMCA/Properties/Resources.Designer.cs
+++ YMCA/Properties/Resources.Designer.cs
@@ -59,25 +59,5 @@ namespace YMCA.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap image {
-            get {
-                object obj = ResourceManager.GetObject("image", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Начать_обработку {
-            get {
-                object obj = ResourceManager.GetObject("Начать обработку", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
